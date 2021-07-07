2f452e519e10a67221ad2c330814a94dce26c622
korus <ruslan.kosarev@smedx.com>
2021-07-07 12:14:46 +0300

    refactoring h5utils

--- facenet/h5utils.py
+++ facenet/h5utils.py
@@ -1,28 +1,28 @@
 # coding:utf-8
 __author__ = 'Ruslan N. Kosarev'
 
-import numpy as np
+from typing import Union
 import h5py
 from pathlib import Path
 
 
-def write(file, name, data, mode='a'):
+def write(file: Union[Path, str], path: str, data, mode='a'):
+
     file = Path(file).expanduser()
-    name = str(name)
-    data = np.atleast_1d(data)
 
     with h5py.File(file, mode=mode) as hf:
-        if name in hf:
-            del hf[name]
-        hf.create_dataset(name, data=data, compression='gzip', dtype=data.dtype)
+        hf[path][...] = data
 
 
-def read(file, name, default=None):
-    with h5py.File(str(file), mode='r') as hf:
-        if name in hf:
-            return hf[name][...]
+def read(file, path, **kwargs):
+
+    file = Path(file).expanduser()
+
+    with h5py.File(file, mode='r') as hf:
+        if path in hf:
+            return hf[path][...]
+
+        if 'default' in kwargs.keys():
+            return kwargs['default']
         else:
-            if default is not None:
-                return default
-            else:
-                raise KeyError(f'Invalid key {name} in H5 file {file}')
+            raise KeyError(f'Invalid path {path} in H5 file {file}')

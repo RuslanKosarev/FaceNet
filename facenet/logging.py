41a70381b96803b888db5e1acd307111da34245b
korus <ruslan.kosarev@smedx.com>
2021-07-14 14:08:43 +0300

    add typing

--- facenet/logging.py
+++ facenet/logging.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from loguru import logger
 
 
-def configure_logging(path: Union[Path, str], file_name='log.txt'):
+def configure_logging(path: Union[Path, str], file_name: str='log.txt'):
     """Configure the application logging
     """
     logger.add(Path(path) / file_name)
